7f19c3a7b69f852153b2958dec38372c58903620
progamação.github <christianlima45687@gmail.com>
2025-04-07 14:30:29 +0000

    Carta1/ Carta2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,39 +1,73 @@
 #include <stdio.h>
 
 int main() {
-    int Carta = 1;
-    char estado[10];
-    char codigo[3];
-    char cidade[10];
-    int populacao;
-    float area;
-    int turisticos;
-    
+    char estado1 [50];
+    char codigo1 [50];
+    char cidade1 [50];
+    int populacao1;
+    float area1;
+    int turisticos1;
 
+    char estado2 [50];
+    char codigo2 [50];
+    char cidade2 [50];
+    int populacao2;
+    float area2;
+    int turisticos2;
+
+
+    printf("Carta1\n");
+    
     printf("Digite seu estado: ");
-    scanf("%s", estado);
-    printf("Estado: %s\n ", estado);
+    scanf("%s", estado1);
+    printf("Estado: %s\n ", estado1);
 
-    
     printf("Digite o código:\n ");
-    scanf("%s", &codigo);
-    printf("Código: %s\n ", codigo);
+    scanf("%s", &codigo1);
+    printf("Código: %s\n ", codigo1);
 
     printf("Digite sua cidade: \n");
-    scanf("%s", cidade);
-    printf("Cidade: %s\n", cidade);
+    scanf("%s", cidade1);
+    printf("Cidade: %s\n", cidade1);
 
     printf("Digite a população:\n");
-    scanf("%d", &populacao);
-    printf("População: %d\n", populacao);
+    scanf("%d", &populacao1);
+    printf("População: %d\n", populacao1);
     
     printf("Digite sua Área: \n");
-    scanf("%f", &area);
-    printf("Área: %f\n", area);
+    scanf("%f", &area1);
+    printf("Área: %f\n", area1);
 
     printf("Digite os pontos turisticos: \n");
-    scanf("%d", &turisticos);
-    printf("Números de Pontos Turísticos: \n");
+    scanf("%d", &turisticos1);
+    printf("Números de Pontos Turísticos: %d\n", turisticos1);
 
+
+
+    printf("Carta2\n");
+    
+    printf("Digite seu estado: ");
+    scanf("%s", estado2);
+    printf("Estado: %s\n ", estado2);
+
+    printf("Digite o código:\n ");
+    scanf("%s", &codigo2);
+    printf("Código: %s\n ", codigo2);
+
+    printf("Digite sua cidade: \n");
+    scanf("%s", cidade2);
+    printf("Cidade: %s\n", cidade2);
+
+    printf("Digite a população:\n");
+    scanf("%d", &populacao2);
+    printf("População: %d\n", populacao2);
+    
+    printf("Digite sua Área: \n");
+    scanf("%f", &area2);
+    printf("Área: %f\n", area2);
+
+    printf("Digite os pontos turisticos: \n");
+    scanf("%d", &turisticos2);
+    printf("Números de Pontos Turísticos: %d \n", turisticos2);
 
 }
